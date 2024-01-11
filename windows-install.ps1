d1cbf8f13465a50a16110b0b5a002589c65a9419
Omkar Dhomane <47618402+dhomane@users.noreply.github.com>
2024-01-11 10:25:39 +0530

    Update windows-install.ps1

--- windows-install.ps1
+++ windows-install.ps1
@@ -6,6 +6,7 @@ if ($agentService) {
     Restart-Service -Name al_agent
     Get-Service -Name al_agent
     Set-Service -Name al_agent -StartupType Automatic
+    echo "AlertLogic Agent already installed and running, exiting script..."
     exit
 }
 
